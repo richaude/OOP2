9bbc3963e6c825939361751c5fb97e318e630efe
richaude <38363696+richaude@users.noreply.github.com>
2018-04-30 21:02:10 +0200

    Add files via upload
    Stand Montagabend, außer in der Spiegelzahl werden die Dokumentationen noch ergänzt

---
+++ Main.java
@@ -0,0 +1,13 @@
+package spiegelzahlen;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Steuerung steuerung = new Steuerung();
+		steuerung.steuern();
+	}
+
+}
